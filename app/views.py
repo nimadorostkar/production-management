b16a805f9b2bcbf45f855caf0cd0aa9249740bd2
nimadorostkar <nimadorostkar97@gmail.com>
2021-07-14 16:24:58 +0430

    nnn

--- app/views.py
+++ app/views.py
@@ -148,9 +148,11 @@ def materials_detail(request, id):
     material = get_object_or_404(models.Material, id=id)
     bom = models.Bom_material.objects.filter(relatedProduct=material)
     stations = models.Station.objects.filter(input_material__material__name=material.name, position='نیرو خانگی'or'ایستگاه'or'برون سپاری')
-    exit_station = models.Station.objects.filter(output_material__name=material.name, position='نیرو خانگی'or'ایستگاه'or'برون سپاری' )
+    exit_station = models.Station.objects.filter(output_material__name=material.name)
     bom_material = models.Bom_material.objects.filter(name__name=material.name)
     tree = models.Tree.objects.filter(station__input_material__material__name=material.name)
+
+    print(exit_station)
 
     context = {
     'material': material,
