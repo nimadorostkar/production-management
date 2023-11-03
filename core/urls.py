08d5903d7bbfbf0863b18f66c901d1cfd33c6816
nima <54316106+nimadorostkar@users.noreply.github.com>
2023-11-03 14:11:50 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
